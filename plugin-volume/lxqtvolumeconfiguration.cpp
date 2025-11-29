b44a504df26d16bff3a0d969756cc034cff86621
Paul <p@1g4.org>
2025-11-29 05:50:46 -0600

    refactor: modernize volume configuration dialog for Linux
    - add explicit Qt widget and utility includes used by the implementation
    - change setSinkList to take the sink list by const reference
    - rebuild sink combo box from AudioDevice list while preserving selection by stored device index
    - simplify audio engine selection logic while keeping backend-specific options
    - keep notification settings logic intact with clearer structure and const locals

--- plugin-volume/lxqtvolumeconfiguration.cpp
+++ plugin-volume/lxqtvolumeconfiguration.cpp
@@ -1,37 +1,16 @@
-/* BEGIN_COMMON_COPYRIGHT_HEADER
- * (c)LGPL2+
- *
- * LXQt - a lightweight, Qt based, desktop toolset
- * https://lxqt.org
- *
- * Copyright: 2010-2011 Razor team
- * Authors:
- *   Alexander Sokoloff <sokoloff.a@gmail.com>
- *
- * This program or library is free software; you can redistribute it
- * and/or modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
-
- * You should have received a copy of the GNU Lesser General
- * Public License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301 USA
- *
- * END_COMMON_COPYRIGHT_HEADER */
-
 #include "lxqtvolumeconfiguration.h"
 #include "ui_lxqtvolumeconfiguration.h"
 
 #include "audiodevice.h"
 
 #include <QComboBox>
-#include <QDebug>
+#include <QDialogButtonBox>
+#include <QCheckBox>
+#include <QLineEdit>
+#include <QSpinBox>
+#include <QRadioButton>
+#include <QAbstractButton>
+#include <QVariant>
 
 LXQtVolumeConfiguration::LXQtVolumeConfiguration(PluginSettings *settings, bool ossAvailable, QWidget *parent) :
     LXQtPanelPluginConfigDialog(settings, parent),
@@ -54,6 +33,7 @@ LXQtVolumeConfiguration::LXQtVolumeConfiguration(PluginSettings *settings, bool 
         connect(ui->ossRadioButton, &QRadioButton::toggled, this, &LXQtVolumeConfiguration::audioEngineChanged);
     else
         ui->ossRadioButton->setVisible(false);
+
 #ifdef USE_PULSEAUDIO
     connect(ui->pulseAudioRadioButton, &QRadioButton::toggled, this, &LXQtVolumeConfiguration::audioEngineChanged);
 #else
@@ -72,20 +52,27 @@ LXQtVolumeConfiguration::~LXQtVolumeConfiguration()
     delete ui;
 }
 
-void LXQtVolumeConfiguration::setSinkList(const QList<AudioDevice *> sinks)
+void LXQtVolumeConfiguration::setSinkList(const QList<AudioDevice *> &sinks)
 {
-    // preserve the current index, as we change the list
-    int tmp_index = settings().value(QStringLiteral(SETTINGS_DEVICE), SETTINGS_DEFAULT_DEVICE).toInt();
+    // preserve selection based on stored device index while updating the list
+    const QVariant savedIndex = settings().value(QStringLiteral(SETTINGS_DEVICE), SETTINGS_DEFAULT_DEVICE);
 
-    const bool old_block = ui->devAddedCombo->blockSignals(true);
+    const bool oldBlock = ui->devAddedCombo->blockSignals(true);
     ui->devAddedCombo->clear();
 
-    for (const AudioDevice *dev : std::as_const(sinks)) {
+    for (const AudioDevice *dev : std::as_const(sinks))
         ui->devAddedCombo->addItem(dev->description(), dev->index());
+
+    int comboIndex = 0;
+    for (int i = 0; i < ui->devAddedCombo->count(); ++i) {
+        if (ui->devAddedCombo->itemData(i) == savedIndex) {
+            comboIndex = i;
+            break;
+        }
     }
 
-    ui->devAddedCombo->setCurrentIndex(tmp_index);
-    ui->devAddedCombo->blockSignals(old_block);
+    ui->devAddedCombo->setCurrentIndex(comboIndex);
+    ui->devAddedCombo->blockSignals(oldBlock);
 }
 
 void LXQtVolumeConfiguration::audioEngineChanged(bool checked)
@@ -94,19 +81,18 @@ void LXQtVolumeConfiguration::audioEngineChanged(bool checked)
         return;
 
     bool canIgnoreMaxVolume = false;
-    if (ui->pulseAudioRadioButton->isChecked())
-    {
+
+    if (ui->pulseAudioRadioButton->isChecked()) {
         if (!mLockSettingChanges)
             settings().setValue(QStringLiteral(SETTINGS_AUDIO_ENGINE), QStringLiteral("PulseAudio"));
         canIgnoreMaxVolume = true;
-    }
-    else if (!mLockSettingChanges)
-    {
-        if(ui->alsaRadioButton->isChecked())
+    } else if (!mLockSettingChanges) {
+        if (ui->alsaRadioButton->isChecked())
             settings().setValue(QStringLiteral(SETTINGS_AUDIO_ENGINE), QStringLiteral("Alsa"));
         else
             settings().setValue(QStringLiteral(SETTINGS_AUDIO_ENGINE), QStringLiteral("Oss"));
     }
+
     ui->ignoreMaxVolumeCheckBox->setEnabled(canIgnoreMaxVolume);
 }
 
@@ -144,6 +130,7 @@ void LXQtVolumeConfiguration::alwaysShowNotificationsCheckBoxChanged(bool state)
 {
     if (!mLockSettingChanges)
         settings().setValue(QStringLiteral(SETTINGS_ALWAYS_SHOW_NOTIFICATIONS), state);
+
     // since always showing notifications is the sufficient condition for showing them with keyboard,
     // self-consistency requires setting the latter to true whenever the former is toggled by the user
     ui->showKeyboardNotificationsCheckBox->setEnabled(!state);
@@ -159,12 +146,16 @@ void LXQtVolumeConfiguration::showKeyboardNotificationsCheckBoxChanged(bool stat
         settings().setValue(QStringLiteral(SETTINGS_SHOW_KEYBOARD_NOTIFICATIONS), state);
 }
 
-
 void LXQtVolumeConfiguration::loadSettings()
 {
     mLockSettingChanges = true;
 
-    QString engine = settings().value(QStringLiteral(SETTINGS_AUDIO_ENGINE), QStringLiteral(SETTINGS_DEFAULT_AUDIO_ENGINE)).toString().toLower();
+    const QString engine = settings()
+                               .value(QStringLiteral(SETTINGS_AUDIO_ENGINE),
+                                      QStringLiteral(SETTINGS_DEFAULT_AUDIO_ENGINE))
+                               .toString()
+                               .toLower();
+
     if (engine == QLatin1String("pulseaudio"))
         ui->pulseAudioRadioButton->setChecked(true);
     else if (engine == QLatin1String("alsa"))
@@ -173,26 +164,34 @@ void LXQtVolumeConfiguration::loadSettings()
         ui->ossRadioButton->setChecked(true);
 
     // currently, this option is only supported by the pulse audio backend
-    if(!ui->pulseAudioRadioButton->isChecked())
+    if (!ui->pulseAudioRadioButton->isChecked())
         ui->ignoreMaxVolumeCheckBox->setEnabled(false);
 
-    setComboboxIndexByData(ui->devAddedCombo, settings().value(QStringLiteral(SETTINGS_DEVICE), SETTINGS_DEFAULT_DEVICE), 1);
-    ui->muteOnMiddleClickCheckBox->setChecked(settings().value(QStringLiteral(SETTINGS_MUTE_ON_MIDDLECLICK), SETTINGS_DEFAULT_MUTE_ON_MIDDLECLICK).toBool());
-    ui->mixerLineEdit->setText(settings().value(QStringLiteral(SETTINGS_MIXER_COMMAND), QStringLiteral(SETTINGS_DEFAULT_MIXER_COMMAND)).toString());
-    ui->stepSpinBox->setValue(settings().value(QStringLiteral(SETTINGS_STEP), SETTINGS_DEFAULT_STEP).toInt());
-    ui->ignoreMaxVolumeCheckBox->setChecked(settings().value(QStringLiteral(SETTINGS_IGNORE_MAX_VOLUME), SETTINGS_DEFAULT_IGNORE_MAX_VOLUME).toBool());
-    ui->alwaysShowNotificationsCheckBox->setChecked(settings().value(QStringLiteral(SETTINGS_ALWAYS_SHOW_NOTIFICATIONS), SETTINGS_DEFAULT_ALWAYS_SHOW_NOTIFICATIONS).toBool());
+    setComboboxIndexByData(ui->devAddedCombo,
+                           settings().value(QStringLiteral(SETTINGS_DEVICE), SETTINGS_DEFAULT_DEVICE),
+                           1);
+
+    ui->muteOnMiddleClickCheckBox->setChecked(
+        settings().value(QStringLiteral(SETTINGS_MUTE_ON_MIDDLECLICK), SETTINGS_DEFAULT_MUTE_ON_MIDDLECLICK).toBool());
+    ui->mixerLineEdit->setText(
+        settings().value(QStringLiteral(SETTINGS_MIXER_COMMAND), QStringLiteral(SETTINGS_DEFAULT_MIXER_COMMAND)).toString());
+    ui->stepSpinBox->setValue(
+        settings().value(QStringLiteral(SETTINGS_STEP), SETTINGS_DEFAULT_STEP).toInt());
+    ui->ignoreMaxVolumeCheckBox->setChecked(
+        settings().value(QStringLiteral(SETTINGS_IGNORE_MAX_VOLUME), SETTINGS_DEFAULT_IGNORE_MAX_VOLUME).toBool());
+    ui->alwaysShowNotificationsCheckBox->setChecked(
+        settings().value(QStringLiteral(SETTINGS_ALWAYS_SHOW_NOTIFICATIONS), SETTINGS_DEFAULT_ALWAYS_SHOW_NOTIFICATIONS).toBool());
+
     // always showing notifications is the sufficient condition for showing them with keyboard
-    if (ui->alwaysShowNotificationsCheckBox->isChecked())
-    {
+    if (ui->alwaysShowNotificationsCheckBox->isChecked()) {
         ui->showKeyboardNotificationsCheckBox->setChecked(true);
         ui->showKeyboardNotificationsCheckBox->setEnabled(false);
-    }
-    else
-    {
-        ui->showKeyboardNotificationsCheckBox->setChecked(settings().value(QStringLiteral(SETTINGS_SHOW_KEYBOARD_NOTIFICATIONS), SETTINGS_DEFAULT_SHOW_KEYBOARD_NOTIFICATIONS).toBool());
+    } else {
+        ui->showKeyboardNotificationsCheckBox->setChecked(
+            settings().value(QStringLiteral(SETTINGS_SHOW_KEYBOARD_NOTIFICATIONS),
+                             SETTINGS_DEFAULT_SHOW_KEYBOARD_NOTIFICATIONS)
+                .toBool());
     }
 
     mLockSettingChanges = false;
 }
-
